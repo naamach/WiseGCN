76ba03cd3512444dbb000e71150ec4dcfa00f9a9
Na'ama Hallakoun <naama@wise.tau.ac.il>
2018-12-23 17:12:08 +0200

    bug fix

--- wisegcn/handler.py
+++ wisegcn/handler.py
@@ -81,7 +81,7 @@ def process_gcn(payload, root):
     try:
         send_mail(subject="[GW@Wise] LVC alert received",
                   text="Attached GCN/LVC alert {} received, started processing.".format(ivorn),
-                  files=filename+'.xml')
+                  files=[filename+'.xml'])
     except:
         print("Failed to sent email!")
         pass
